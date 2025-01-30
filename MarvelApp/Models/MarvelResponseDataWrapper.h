52cf18992e44c56517ba056f2a9996003e2caa1f
Amr Elghadban <amr.elghadban@gmail.com>
2025-01-30 09:29:04 +0400

    Update MarvelResponseDataWrapper.h

--- MarvelApp/Models/MarvelResponseDataWrapper.h
+++ MarvelApp/Models/MarvelResponseDataWrapper.h
@@ -9,24 +9,19 @@
 #import <Foundation/Foundation.h>
 
 @interface MarvelResponseDataWrapper : NSObject
+
 @property (nonatomic, strong) NSNumber * code ;
 @property (nonatomic, strong) NSString * status;
-@property (nonatomic, strong) NSString * copyright ;
-@property (nonatomic, strong) NSString * attributionText ;
-@property (nonatomic, strong) NSString * attributionHTML ;
+@property (nonatomic, strong) NSString * copyright;
+@property (nonatomic, strong) NSString * attributionText;
+@property (nonatomic, strong) NSString * attributionHTML;
 @property (nonatomic, strong) NSString * etag ;
-
-
 
 @property (nonatomic, strong) NSNumber * count ;
 @property (nonatomic, strong) NSNumber * limit ;
 @property (nonatomic, strong) NSNumber * offset;
 
-
 @property (nonatomic, strong) NSArray * results ;
-
-
-
 
 -(instancetype)initWithDictionary:(NSDictionary *)Dictionary;
 
