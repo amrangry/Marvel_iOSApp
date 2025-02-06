fc5005e7abc9a7140adbd8219d2838c377bd49bd
Amr Elghadban <amr.elghadban@gmail.com>
2025-02-06 13:48:27 +0400

    Update PopUpViewController.m

--- MarvelApp/ViewControllers/PopUpViewController/PopUpViewController.m
+++ MarvelApp/ViewControllers/PopUpViewController/PopUpViewController.m
@@ -15,8 +15,6 @@
 
 @implementation PopUpViewController
 
-
-
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
@@ -24,21 +22,15 @@
 
 
 -(void)viewWillAppear:(BOOL)animated{
-
-
     [super viewWillAppear:animated];
     
     _displayImage.image=_img;
-    
-    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-
 
 - (IBAction)skipBtnPressed:(id)sender {
     
